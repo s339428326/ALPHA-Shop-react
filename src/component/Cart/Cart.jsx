6e40365d4f5582a776eb61897c05e90a6e658d0e
翁柏豐 <s339428326@gmail.com>
2022-11-07 16:50:46 +0800

    feat:add Cart none CartItem View

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,19 +1,6 @@
+import styles from "./Cart.module.scss";
 import CartItem from "../CartItem/CartItem";
 import { useState } from "react";
-// {
-//   id: "1",
-//   name: "貓咪罐罐",
-//   img: "https://picsum.photos/300/300?text=1",
-//   price: 100,
-//   quantity: 2,
-// },
-// {
-//   id: "2",
-//   name: "貓咪干干",
-//   img: "https://picsum.photos/300/300?text=2",
-//   price: 200,
-//   quantity: 1,
-// },
 
 export default function Cart(props) {
   console.log("Cart資料-", props.data);
@@ -22,36 +9,57 @@ export default function Cart(props) {
     props.data.map((item) => item.price * item.quantity).reduce((a, b) => a + b)
   );
 
+  // {props.data.map((item, index) => {
+  //   return (
+  //     <li key={`CartItem-${index}`}>
+  //       <CartItem
+  //         id={item.id}
+  //         name={item.name}
+  //         img={item.img}
+  //         price={item.price}
+  //         quantity={item.quantity}
+  //         data={props.data}
+  //         setData={props.setData}
+  //         setCal={setCal}
+  //       />
+  //     </li>
+  //   );
+  // })}
   return (
     <div className="px-24 py-32 border rounded-3 h-100">
       <h3>購物籃</h3>
       <ul className="list-unstyled d-flex flex-column gap-24 my-32">
-        {props.data.map((item, index) => {
-          return (
-            <li key={`CartItem-${index}`}>
-              <CartItem
-                id={item.id}
-                name={item.name}
-                img={item.img}
-                price={item.price}
-                quantity={item.quantity}
-                data={props.data}
-                setData={props.setData}
-                setCal={setCal}
-              />
-            </li>
-          );
-        })}
+        {cal !== 0 ? (
+          props.data.map((item, index) => {
+            return (
+              <li key={`CartItem-${index}`}>
+                <CartItem
+                  id={item.id}
+                  name={item.name}
+                  img={item.img}
+                  price={item.price}
+                  quantity={item.quantity}
+                  data={props.data}
+                  setData={props.setData}
+                  setCal={setCal}
+                />
+              </li>
+            );
+          })
+        ) : (
+          <div className={`${styles["item-none"]}`}>
+            <p>目前沒有任何商品</p>
+          </div>
+        )}
       </ul>
 
       <div className="d-flex justify-content-between pt-16 border-top mb-32">
         <p>運費</p>
-        {/* 這裡需要接取From的Step2 運送金額 */}
         <strong>{props.deliveryFee === 0 ? "免費" : props.deliveryFee}</strong>
       </div>
       <div className="d-flex justify-content-between pt-16 border-top">
         <p>小計</p>
-        <strong>$ {cal + props.deliveryFee}</strong>
+        <strong>$ {cal !== 0 ? cal + props.deliveryFee : cal}</strong>
       </div>
     </div>
   );
