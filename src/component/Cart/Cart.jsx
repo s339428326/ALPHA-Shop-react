2b71a11533b50f58c635610a9f06661d566d2000
翁柏豐 <s339428326@gmail.com>
2022-11-11 22:41:49 +0800

    feat:add currency style

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -10,43 +10,55 @@ import { useState } from "react";
 import { useContext } from "react";
 
 export default function Cart(props) {
-  //context var
+  //Context
   const cartData = useContext(CartContext);
   const paymentData = useContext(PaymentContext);
-  console.log("Cart資料-", cartData);
+  console.log("Cart資料-CartContext", cartData);
+
+  //貨幣格式
+  const currency = function (number) {
+    return Intl.NumberFormat("en").format(number);
+  };
 
   //總金額
   const [cal, setCal] = useState(
     cartData.map((item) => item.price * item.quantity).reduce((a, b) => a + b)
   );
+
+  const totalPrice = cal !== 0 ? cal + props.deliveryFee : cal;
+
   //paymentData paymentPrice setting
-  paymentData.paymentPrice = cal !== 0 ? cal + props.deliveryFee : cal;
+  paymentData.paymentPrice = totalPrice;
   console.log(paymentData);
+
+  //cartList
+  const listCount = cartData.filter((item) => item.quantity > 0).length;
+  const cartList = listCount ? (
+    cartData.map((item, index) => (
+      <li key={`CartItem-${index}`}>
+        <CartItem
+          id={item.id}
+          name={item.name}
+          img={item.img}
+          price={item.price}
+          quantity={item.quantity}
+          setCal={setCal}
+          setItemCount={props.setItemCount}
+          currency={currency}
+        />
+      </li>
+    ))
+  ) : (
+    <div className={`${styles["item-none"]}`}>
+      <p>目前沒有任何商品</p>
+    </div>
+  );
+
   return (
     <div className="px-24 py-32 border rounded-3 h-100">
       <h3>購物籃</h3>
       <ul className="list-unstyled d-flex flex-column gap-24 my-32 item-list">
-        {cal !== 0 ? (
-          cartData.map((item, index) => {
-            return (
-              <li key={`CartItem-${index}`}>
-                <CartItem
-                  id={item.id}
-                  name={item.name}
-                  img={item.img}
-                  price={item.price}
-                  quantity={item.quantity}
-                  setCal={setCal}
-                  setItemCount={props.setItemCount}
-                />
-              </li>
-            );
-          })
-        ) : (
-          <div className={`${styles["item-none"]}`}>
-            <p>目前沒有任何商品</p>
-          </div>
-        )}
+        {cartList}
       </ul>
 
       <div className="d-flex justify-content-between pt-16 border-top mb-32">
@@ -55,7 +67,7 @@ export default function Cart(props) {
       </div>
       <div className="d-flex justify-content-between pt-16 border-top">
         <p>小計</p>
-        <strong>$ {cal !== 0 ? cal + props.deliveryFee : cal}</strong>
+        <strong>$ {currency(totalPrice)}</strong>
       </div>
     </div>
   );
